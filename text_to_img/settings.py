92f30da171347ddeaa4070c8fe5356652b0de986
DebugManager <116737219+YuraYarmola@users.noreply.github.com>
2023-11-06 12:54:16 +0200

    Fix noise

--- text_to_img/settings.py
+++ text_to_img/settings.py
@@ -78,6 +78,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'text_to_img.urls'
@@ -207,9 +208,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'text_to_img/static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'text_to_img/static')]
 
 
 
