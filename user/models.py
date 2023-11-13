92107045e69230b1ffab4bb116ac89e0664d97fb
DebugManager <markovpython2@gmail.com>
2023-11-13 18:25:03 +0200

    Reload db

--- user/models.py
+++ user/models.py
@@ -7,17 +7,6 @@ from main.models import Plan, Company
 from .managers import CustomUserManager
 
 
-class Affiliate(models.Model):
-    promotion_plan = models.TextField(blank=True, null=True)
-    twitter = models.CharField(max_length=255, blank=True, null=True)
-    instagram = models.CharField(max_length=255, blank=True, null=True)
-    tiktok = models.CharField(max_length=255, blank=True, null=True)
-    linkedin = models.CharField(max_length=255, blank=True, null=True)
-    facebook = models.CharField(max_length=255, blank=True, null=True)
-    paypal_email = models.CharField(max_length=100, blank=True, null=True)
-    btc_adress = models.CharField(max_length=255, blank=True, null=True)
-
-
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     username = None
 
@@ -31,7 +20,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     customer_id = models.CharField(max_length=100, default="Clear")
     payment_method_id = models.CharField(max_length=100, default=None, blank=True, null=True)
     subscription_id = models.CharField(max_length=100, default=None, blank=True, null=True)
-    affiliate_id = models.ForeignKey(Affiliate, on_delete=models.CASCADE, blank=True, null=True)
+    affiliate_id = models.ForeignKey('Affiliate', on_delete=models.CASCADE, blank=True, null=True)
 
     objects = CustomUserManager()
     first_name = models.CharField(max_length=50)
@@ -62,6 +51,18 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     class Meta:
         permissions = [('view', 'Can view specific page'), ('edit', 'Can edit content'), ('comment', 'Can comment'),
                        ('create_new', 'Can create new user')]
+
+
+class Affiliate(models.Model):
+    promotion_plan = models.TextField(blank=True, null=True)
+    twitter = models.CharField(max_length=255, blank=True, null=True)
+    instagram = models.CharField(max_length=255, blank=True, null=True)
+    tiktok = models.CharField(max_length=255, blank=True, null=True)
+    linkedin = models.CharField(max_length=255, blank=True, null=True)
+    facebook = models.CharField(max_length=255, blank=True, null=True)
+    paypal_email = models.CharField(max_length=100, blank=True, null=True)
+    btc_adress = models.CharField(max_length=255, blank=True, null=True)
+    affiliated_users = models.ManyToManyField(CustomUser, related_name='affiliate_membership')
 
 
 class Feature(models.Model):
--- user/urls.py
+++ user/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include, re_path
 from user.views import CustomTokenCreateView, GrantPermissionView, UserRoleList, \
     CreateUserAndGrantPermissionView, UpdateUserAndPermissionsView, UserDetail, AllUsersForAdminView, \
     DetailUserForAdminView, AllTicketForAdminView, PersonalInfoUpdade, ChatRoomListCreateView, ChatRoomDetailView, \
-    ChatMessagesView, CustomUserCreateView, UserList, AffiliateEdit
+    ChatMessagesView, CustomUserCreateView, UserList, AffiliateEdit, AffiliateListView
 
 urlpatterns = [
     path('v1/user-create-with-permissions/', CreateUserAndGrantPermissionView.as_view()),
@@ -17,6 +17,7 @@ urlpatterns = [
     path('v1/admin/users/', AllUsersForAdminView.as_view()),
     path('v1/admin/users/<int:pk>/', DetailUserForAdminView.as_view()),
     path('v1/admin/ticket/', AllTicketForAdminView.as_view()),
+    path('v1/admin/affiliates/', AffiliateListView.as_view()),
     path('v1/create-room/', ChatRoomListCreateView.as_view()),
     path('v1/room/<str:name>/', ChatRoomDetailView.as_view()),
     path('v1/messages/<str:room_name>/', ChatMessagesView.as_view()),
